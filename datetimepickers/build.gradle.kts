9980924eaeaf4e34880ae1849af45c1291d9c41b
Maroš Šeleng <maros@marosseleng.com>
2023-04-18 17:41:59 +0200

    Prepare v0.7.1.

--- datetimepickers/build.gradle.kts
+++ datetimepickers/build.gradle.kts
@@ -87,7 +87,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.marosseleng.android"
             artifactId = "compose-material3-datetime-pickers"
-            version = "0.7.0"
+            version = "0.7.1"
 
             afterEvaluate {
                 from(components["release"])
